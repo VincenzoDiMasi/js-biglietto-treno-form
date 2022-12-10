aba6fd752c45a064910b79dea016f629f43e372c
VincenzoDiMasi <v.dimasi93@gmail.com>
2022-12-10 19:28:18 +0100

    Finish

--- script.js
+++ script.js
@@ -17,34 +17,69 @@ Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagi
 
 console.log ('JS OK')
 
-//1-Prendere l'elemento dal DOM
+// PRENDO GLI ELEMENTI DAL DOM
+const nameField = document.getElementById ('name');
+const kmsField = document.getElementById ('kms');
+const ageField = document.getElementById ('age');
+const confirmButton = document.getElementById ('confirm-button');
+const resetmButton = document.getElementById ('reset-button');
+const ticketSection = document.getElementById ('ticket-section');
+const passengerNameElement = document.getElementById ('passenger-name');
+const rateElement = document.getElementById ('rate');
+const coachElement = document.getElementById ('coach');
+const pnrElement = document.getElementById ('pnr');
+const priceElement = document.getElementById ('price');
 
-const bttn = document.getElementById ('button');
-const definitivePrice = document.getElementById ('target');
-const runKms = document.getElementById ('kms');
-const currentAge = document.getElementById ('age');
+// COLLEGO L'EVENT LISTENER AL BUTTON
+confirmButton.addEventListener('click', function(){
+    const nameValue = nameField.value.trim();
+    const kmsValue = parseInt(kmsField.value.trim());
+    const ageValue = ageField.value;
+
+    //Validazione 
+    if(!nameValue || isNaN(kmsValue) || kmsValue < 10){
+        alert('Valori non validi!');
+        return;
+    } 
+
+    //Calcolo prezzo
+    let price = 0.21 * kmsValue;
+    let rateName = 'Tariffa Ordinaria';
+
+    if(ageValue === 'under') {
+        price = price * 0.8;
+        rateName = 'Tariffa Giovani';
+    } else if(ageValue === 'over') {
+        price = price * 0.6;
+        rateName = 'Tariffa Anziani';
+    }
+
+    console.log(price, rateName);
+
+    //Arrotondo
+    price = price.toFixed(2) + '€';
+
+    //Coach & PNR
+    const coach = '9'
+    const pnr = '261293'
+
+    //Monto i dati nell html
+    passengerNameElement.innerText = nameValue;
+    rateElement.innerText = rateName;
+    coachElement.innerText = coach;
+    pnrElement.innerText = pnr;
+    priceElement.innerText = price;
+
+    //Tolgo il d-none alla ticket-section
+    ticketSection.classList.remove('d-none');
+
+    //Svuoto i campi dopo il click 
+    nameField.value = '';
+    kmsField.value = '10';
+    ageField.value = '';
 
 
-//Creo una costante col prezzo/km del viaggio
-const tariff = 0.21;
-
-//Calcolo 
-bttn.addEventListener('click', function(){
-    //prendo i value dalla pagina
-    const rideKm = parseInt(kms.value)
-    const passengerAge = parseInt(age.value)
-
-    //calcolo costo viaggio
-    let ridePrice = rideKm * tariff
-     
-
-    //creo le costanti per la scontistica
-    const underSale = 20;
-    const overSale = 40;
-   
-    definitivePrice.innerText = ` Il prezzo del biglietto e di ${ridePrice}€`
 
 })
-
 
 
